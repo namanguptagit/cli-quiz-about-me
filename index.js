35725250a3d06d1933e54ebfab837c1708c9f655
Naman Gupta <naman312000@gmail.com>
2021-09-10 05:51:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+var readlineSync = require('readline-sync');
+var userName=readlineSync.question("what's your name ? ");
+var score=0;
+console.log("welcome "+userName+" to DO YOU KNOW Naman");
+function play(question,answer)
+{
+  var userAnswer=readlineSync.question(question); 
+  if(userAnswer==answer)
+  {
+    console.log("yes you are right!");
+    score++;
+  }
+  else
+  {
+    console.log("no you are wrong!");
+  }
+    console.log("current score = "+ score);
+}
+var questions=[
+  {
+  question:"where do i live ",
+  answer:"moradabad",
+},
+{
+  question:"where i want to go ",
+  answer:"usa",
+},
+{
+  question:"when is my birthday ",
+  answer:"3january",
+
+}
+];
+for(var i=0;i<questions.length;i++)
+{
+  var currentQuestion=questions[i];
+  play(currentQuestion.question,currentQuestion.answer);
+}
+console.log("Final score is = "+score);
